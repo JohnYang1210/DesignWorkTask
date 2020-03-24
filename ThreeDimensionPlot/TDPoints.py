5fe6fe8d03d51e47ddede4735d2c4f139905262f
JohnYang <1574891843@qq.com>
2020-03-24 13:47:39 +0800

    FilterType of terrain line is extended with AcDb3dPolyline

--- ThreeDimensionPlot/TDPoints.py
+++ ThreeDimensionPlot/TDPoints.py
@@ -1,14 +1,14 @@
 import os,sys
 os.chdir(r'F:\PycharmProject\PycomCAD')
 sys.path.append(r'F:\PycharmProject\PycomCAD')
-f=open('3DPointCloud.txt','w')
+f=open('3DPointCloudYueNan.txt','w')
 from pycomcad import *
 
 acad=Autocad()
 print('Connected ...')
 ft=vtInt([8])
-fd=vtVariant(['dxt'])
-slt=acad.GetSelectionSets('slt')
+fd=vtVariant(['SRTM_Terrain_data_shp'])
+slt=acad.GetSelectionSets('slt3')
 slt.SelectOnScreen(ft,fd)
 for i in range(slt.Count):
 	if slt.Item(i).EntityName=='AcDbPolyline':
@@ -19,6 +19,16 @@ for i in range(slt.Count):
 			data=str(cache.Coordinate(j)[0])+','+str(cache.Coordinate(j)[1])+','+ele+'\n'
 			f.write(data)
 		cache.Delete()
+	elif slt.Item(i).EntityName=='AcDb3dPolyline':
+		obj=slt.Item(i)
+		j=0
+		while True:
+			try:
+				data=str(obj.Coordinate(j)[0])+','+str(obj.Coordinate(j)[1])+','+str(obj.Coordinate(j)[2])+'\n'
+				f.write(data)
+				j=j+1
+			except:
+				break
 	else:
 		print('Not a polyline...')
 
