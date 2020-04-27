266c9159216808efe48e19cbe401ee62bebbfcf3
JohnYang <1574891843@qq.com>
2020-04-27 17:20:00 +0800

    optimize sjqy2midas and add explore2.ipynb as an experiment notebook file for further and deeper development of midas operation.

--- sjqy2midas/sjqy2midas.py
+++ sjqy2midas/sjqy2midas.py
@@ -5,14 +5,14 @@ import re
 import os
 import pandas as pd 
 import io
-MEDIATEFILENAME='elementRawSet.csv'
+MEDIATEFILENAME_Element='elementRawSet.csv'
+MEDIATEFILENAME_Node='nodeRawSet.csv'
 class Model:
-	def __init__(self,file):
+	def __init__(self,file,dele=False):
 		"""
 		By default,dele=False,if set True,related mediate file will be deleted.
 		
 		"""
-		
 		filePath=os.path.split(file)
 		self.path=filePath[0]
 		self.filename=os.path.join(self.path,filePath[1].split('.')[0]+'.mgt')
@@ -20,46 +20,55 @@ class Model:
 			self.content=f.read()
 		self.nodeSet=self.GetNodeSet()
 		self.elemSet=self.GetEleSet()
+		if dele:
+			os.remove(self.mediateFilenameElem)
+			os.remove(self.mediateFilenameNode)
 		
 	def GetNodeSet(self):
 		nodeStr=re.findall(r'(?s)JOINT\s+(.*)SPRING',self.content)[0]
 		pattr=re.compile(r'(?s)[X=|Y=|Z=]')
 		nodeSet=re.sub(pattr,'',nodeStr)
 		nodeSet=nodeSet.replace(' ',',')
+		self.mediateFilenameNode=os.path.join(self.path,MEDIATEFILENAME_Node)
+		with open(self.mediateFilenameNode,'w') as f:
+			f.write(nodeSet)
+		self.nodeDf=pd.read_csv(self.mediateFilenameNode,engine='python',names=['node num','x','y','z'])
+		self.nodeDf.set_index('node num',inplace=True)
 		return nodeSet
 
 	def GetEleSet(self):
 		eleStr=re.findall(r'(?s)SHELL SECTION\s+(.*)SHELL\s+(.*)LOAD',self.content)[0][1]
 		pattr=re.compile(r'(?s)[J=|(SEC=SSEC)]')
 		rawEleSet=re.sub(pattr,'',eleStr).replace(' ',',')
-		self.mediateFilename=os.path.join(self.path,MEDIATEFILENAME)
-		with open(self.mediateFilename,'w') as f:
+		self.mediateFilenameElem=os.path.join(self.path,MEDIATEFILENAME_Element)
+		with open(self.mediateFilenameElem,'w') as f:
 			f.write(rawEleSet)
-		elemDf=pd.read_csv(self.mediateFilename,engine='python',names=['element number','elem1','elem2','elem3','elem4','section num'])
+		elemDf=pd.read_csv(self.mediateFilenameElem,engine='python',names=['element number','Node1','Node2','Node4','Node3','section num'])
+		elemDf=elemDf
 		elemDf['type']='PLATE'
 		elemDf['imat']=1
 		elemDf['ipro']=1
 		elemDf['isub']=3
 		elemDf['iwid']=0
-		order=['element number','type','imat','ipro','elem1','elem2','elem4','elem3']
-		self.elemDf=elemDf[order]
-		self.elemDf.set_index('element number',inplace=True) # attention here,set_index returns a new object unless inplace=True.
+		order=['element number','type','imat','ipro','Node1','Node2','Node3','Node4']
+		elemDf=elemDf[order]
+		self.elemDf=elemDf
+		elemDf.set_index('element number',inplace=True) # attention here,set_index returns a new object unless inplace=True.
 		elemOutputs=io.StringIO()
-		elemOutputs.write(self.elemDf.to_csv(header=False))
+		elemOutputs.write(elemDf.to_csv(header=False))
 		elemSet=elemOutputs.getvalue()
 		elemOutputs.close()
 		return elemSet
-	def gen(self,dele):
+	def gen(self):
 		content='*NODE\n'+self.nodeSet+'*ELEMENT\n'+self.elemSet
 		with open(self.filename,'w')  as f:
 			f.write(content)
-		if dele:
-			os.remove(self.mediateFilename)
+		
 
 
 if __name__=='__main__':
 	inputs=input('input the .s2k file path')
-	Model(inputs).gen(dele=True)
+	Model(inputs,dele=True).gen()
 
 
 
