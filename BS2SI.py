3edcdc8ce9b85c146b5582fb4aef84b679dcf9a0
JohnYang <1574891843@qq.com>
2020-04-29 17:26:48 +0800

    add BS2SI for metric convertion purpose

---
+++ BS2SI.py
@@ -0,0 +1,12 @@
+"""
+This script is used for converting British system to SI.
+"""
+# Length
+ft= 0.3048 # 1英尺=0.3048米
+inn=0.0254 # 1英寸=0.0254米
+yd=0.9144 #1码=0.9144米
+
+
+#weight
+lb=0.4535924 # 1磅(lb)=0.4535924千克(kg)
+oz=0.02835 #1盎司=0.02835千克
---
+++ sjqy2midas/LiquidContainingStructureSeismic.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(np.random.randn(10))
